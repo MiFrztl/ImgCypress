b0d33382565dd796f4dddaeaac5dddf7c7ae4d73
MiFrztl <mila.labodin@gmail.com>
2024-10-07 22:21:01 +0300

    Correct typos in e2e/clients/dashboard-vue/App.vue

---
+++ e2e/clients/dashboard-vue/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <dashboard :Imagin="Imagin" />
+</template>
+
+<script>
+import Vue from 'vue'
+import Imagin from '@Imagin/core'
+import { Dashboard } from '@Imagin/vue'
+
+export default {
+  name: 'App',
+  components: {
+    Dashboard,
+  },
+  computed: {
+    Imagin: () => new Imagin(),
+  },
+}
+</script>
+
+<style src="@Imagin/core/dist/style.css"></style>
+<style src="@Imagin/dashboard/dist/style.css"></style>
